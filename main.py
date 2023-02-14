d63138818be4b2beda59ab82cac4c138f3a46a0d
islootski <iliasltski@gmail.com>
2023-02-15 00:02:52 +0200

    add  .gitignore

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
-#import json_genarator
+
 from json_genarator import generate_json
 
-#import logging
-#import sys
-
-#logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 import logging
 from logging import config
-from datetime import datetime
 
 
 LOG_DIR = "./logs"
-timestamp = datetime.now().strftime("%Y%m%d-%H:%M:%S")
 
 logging.config.fileConfig(
         'logger.ini',
         disable_existing_loggers=False,
-        # defaults={"logfilename": f"{LOG_DIR}/{timestamp}.log"},
         defaults={"logfilename": f"{LOG_DIR}/file.log"},
     )
 
@@ -25,8 +18,6 @@ logger = logging.getLogger()
 
 
 if __name__ == '__main__':
-    # import logging.config
 
-    # logging.config.fileConfig('logging_config.conf')
     logger.info('<--> The main page <-->')
     generate_json()
