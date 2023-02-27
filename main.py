522fa9735a17134f86bc98c1c03b3770b8d2ed6a
islootski <iliasltski@gmail.com>
2023-02-27 14:57:21 +0200

    add curl to parallel object URL

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from json_genarator import generate_json
 
 import logging
 from logging import config
-
+import pycurl
+import certifi
+from io import BytesIO
 
 LOG_DIR = "./logs"
 
@@ -17,7 +19,33 @@ logging.config.fileConfig(
 logger = logging.getLogger()
 
 
-if __name__ == '__main__':
+def curl():
+    buffer = BytesIO()
+    c = pycurl.Curl()
+    c.setopt(c.URL, 'http://host.docker.internal:8000/')
+    c.setopt(c.WRITEDATA, buffer)
+    # setting the file name holding the certificates
+    c.setopt(c.CAINFO, certifi.where())
+    # perform file transfer
+    c.perform()
+    # Ending the session and freeing the resources
+    c.close()
 
+    print("asdfasdfasdfasdf", "asdfasdfasdfasdfa", "asdfalkjasdf;lkj;asdf", "<<<=-=-=-=-=-=-=-=-=-=->>>")
+    # retrieve the content BytesIO
+    body = buffer.getvalue()
+    # decoding the buffer
+    print("\nCurl:")
+    print("<-=-=-=-=-=-\n", "-=-=-=-=-=-", body.decode('iso-8859-1'), "-=-=-=-=-=-\n", "-=-=-=-=-=->")
+
+
+if __name__ == '__main__':
+    import time
+    print("before 20")
+    time.sleep(20)
+    print("after 20")
     logger.info('<--> The main page <-->')
     generate_json()
+
+    curl()
+
